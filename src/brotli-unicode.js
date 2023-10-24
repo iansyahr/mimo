b85ee6cb61dfa1cda3111c39aade2636842f5109
Muhamad Apriansyah Ramadhan <37446681+iansyahr@users.noreply.github.com>
2023-10-24 12:40:54 +0700

    update

--- src/brotli-unicode.js
+++ src/brotli-unicode.js
@@ -1,1 +1,18 @@
-console.log("Hello World")
+import brotli from './index.mjs'
+
+// alternatively, in-browser (without any third-party libraries)
+input = TextEncoder.encode('Hello🤖!')
+
+// it takes a Uint8Array and returns a base-unicode encoded string (copy and pasteable)
+const compressed = await brotli.compress(input)
+console.log(compressed)
+
+// it takes the base-unicode encoded string and returns a Uint8Array
+const decompressed = await brotli.decompress(compressed)
+
+
+export async function compressBrotli(input){
+    let buffer = new TextEncoder().encode(input);
+    const compressed = await compress(buffer);
+    return compressed;
+  }
